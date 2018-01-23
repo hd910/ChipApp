0c9e9bf9dddba2ecee1b819a95832d7e62339fc3
hd910 <hd910@outlook.com>
2018-01-24 11:18:05 +1300

    Set initial components

---
+++ chipapp/src/components/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import GameMessage from './GameMessage';
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                {/* <GameMessage /> */}
+                <div className="container">
+                    
+                    
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ chipapp/src/components/GameMessage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class GameMessage extends Component{
+    render(){
+        return(
+            <h2>GameMessage Here</h2>
+        )
+    }
+}
+
+export default GameMessage;
