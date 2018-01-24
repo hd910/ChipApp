2038126d3f1c097872af9ea42aeb191d42b24c77
hd910 <hd910@outlook.com>
2018-01-24 16:35:45 +1300

    Socket.io test

--- chipapp/src/components/App.js
+++ chipapp/src/components/App.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 import GameMessage from './GameMessage';
+import { subscribeToTimer } from '../api';
 
 class App extends Component{
+    constructor(props){
+        super(props);
+        subscribeToTimer((err, timestamp) => this.setState({ 
+            timestamp 
+        }));
+    }
+    state = {
+        timestamp: 'no timestamp yet'
+    };
     render(){
         return(
             <div>
-                {/* <GameMessage /> */}
                 <div className="container">
-                    <GameMessage></GameMessage>
-                    
+                    {/* <GameMessage></GameMessage> */}
+                    This is the timer value: {this.state.timestamp}
                 </div>
             </div>
         );
